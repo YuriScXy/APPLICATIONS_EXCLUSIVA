8aa11969222c3cbf0b7d3c98aefead5e365c722b
YuriScXy <yurialchemist@gmail.com>
2023-08-03 09:26:55 -0300

    alteração para funcionar no servidor
    Alterei o tipo de certificado pois o servidor é linux e além disso mudei umas questões de rotas

--- ControleCartao/config.php
+++ ControleCartao/config.php
@@ -3,7 +3,7 @@
 $_ENV = parse_ini_file('keys.env');
 
 $mysqli = mysqli_init();
-$mysqli->ssl_set(NULL, NULL, "cacert-2023-05-30.pem", NULL, NULL);
+$mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
 $mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
 $mysqli->close();
 
